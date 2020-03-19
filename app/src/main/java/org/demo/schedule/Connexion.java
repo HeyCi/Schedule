1d2ed821e517a8ffaf5c5f5bdb87c84ac8ea742b
HeyCi <acchevp@gmail.com>
2020-03-19 12:59:02 +0100

    creation des 3 activites de travail

---
+++ app/src/main/java/org/demo/schedule/Connexion.java
@@ -0,0 +1,14 @@
+package org.demo.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Connexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+    }
+}
--- app/src/main/java/org/demo/schedule/MainActivity.java
+++ app/src/main/java/org/demo/schedule/MainActivity.java
@@ -2,13 +2,46 @@ package org.demo.schedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_la;
+    Button btn_ac;
+    Button btn_sg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_la = findViewById(R.id.btn_la);
+        btn_ac = findViewById(R.id.btn_ac);
+        btn_sg = findViewById(R.id.btn_sg);
+
+        btn_la.setOnClickListener(this);
+        btn_ac.setOnClickListener(this);
+        btn_sg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btn_la) {
+            Intent intent = new Intent(this, Connexion.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(view == btn_ac) {
+            Intent intent = new Intent(this, KidSchedule.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(view == btn_sg) {
+            Intent intent = new Intent(this, AdultSchedule.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
