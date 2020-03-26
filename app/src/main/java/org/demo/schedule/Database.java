2a4821c939200e4fd6a855aeca2a30c96926c377
Whiteyeas <gabriel-le-jedi@hotmail.fr>
2020-03-26 11:49:27 +0100

    creation test

--- app/src/main/java/org/demo/schedule/Database.java
+++ app/src/main/java/org/demo/schedule/Database.java
@@ -1,4 +1,21 @@
 package org.demo.schedule;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Database {
+    public void CreateUser(User user){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("User");
+        userRef.setValue(user);
+    }
+
+    public void CreateTask(Task task){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("Task");
+        userRef.setValue(task);
+    }
+
+    public Database() {
+    }
 }
