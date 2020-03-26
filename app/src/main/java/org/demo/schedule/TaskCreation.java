812e1e5c003fde25b0483856643e05cd226524bd
SiegKord <nicolas.ferrer23@gmail.com>
2020-03-26 16:53:28 +0100

    second test for task_creation interface

--- app/src/main/java/org/demo/schedule/TaskCreation.java
+++ app/src/main/java/org/demo/schedule/TaskCreation.java
@@ -1,16 +1,38 @@
 package org.demo.schedule;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TimePicker;
+
+import android.app.TimePickerDialog;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class TaskCreation extends AppCompatActivity {
+
+    Button choose_btn;
+    TimePickerDialog timePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
+
+        choose_btn = findViewById(R.id.btn_hr);
+        choose_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog = new TimePickerDialog(TaskCreation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        choose_btn.setText(hourOfDay + ":" + minute);
+                    }
+                }, 0, 0, false);
+
+                timePickerDialog.show();
+            }
+        });
 
     }
 
