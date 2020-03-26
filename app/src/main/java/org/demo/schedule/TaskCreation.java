745f7dd3bfb098ecd8ad10583a38b53e711c5c08
SiegKord <nicolas.ferrer23@gmail.com>
2020-03-26 18:06:06 +0100

    front interval reminder missing

--- app/src/main/java/org/demo/schedule/TaskCreation.java
+++ app/src/main/java/org/demo/schedule/TaskCreation.java
@@ -9,9 +9,15 @@ import android.widget.Button;
 import android.os.Bundle;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+
 public class TaskCreation extends AppCompatActivity {
 
-    Button choose_btn;
+    Calendar calendar;
+    int currentHour_heure, currentHour_duree;
+    int currentMinute_heure, currentMinute_duree;
+    Button choose_hr_btn;
+    Button choose_duree_btn;
     TimePickerDialog timePickerDialog;
 
     @Override
@@ -19,16 +25,39 @@ public class TaskCreation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
 
-        choose_btn = findViewById(R.id.btn_hr);
-        choose_btn.setOnClickListener(new View.OnClickListener() {
+        choose_hr_btn = findViewById(R.id.btn_hr);
+        choose_hr_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                calendar = Calendar.getInstance();
+                currentHour_heure = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute_heure = calendar.get(Calendar.MINUTE);
                 timePickerDialog = new TimePickerDialog(TaskCreation.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        choose_btn.setText(hourOfDay + ":" + minute);
+                        choose_hr_btn.setText(hourOfDay + ":" + minute);
                     }
-                }, 0, 0, false);
+                }, currentHour_heure, currentMinute_heure, true);
+
+                timePickerDialog.show();
+            }
+        });
+
+        choose_duree_btn = findViewById(R.id.btn_dur);
+        choose_duree_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calendar = Calendar.getInstance();
+                currentHour_duree = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute_duree = calendar.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(TaskCreation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        choose_duree_btn.setText(hourOfDay + ":" + minute);
+                    }
+                }, currentHour_duree, currentMinute_duree, true);
 
                 timePickerDialog.show();
             }
