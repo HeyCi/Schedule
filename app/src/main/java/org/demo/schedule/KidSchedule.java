47d924b08146ea5e106a0a01f8e3d3e3a8d654c7
HeyCi <acchevp@gmail.com>
2020-04-02 11:02:54 +0200

    debut planning avec bdd

--- app/src/main/java/org/demo/schedule/KidSchedule.java
+++ app/src/main/java/org/demo/schedule/KidSchedule.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class KidSchedule extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +23,9 @@ public class KidSchedule extends AppCompatActivity implements View.OnClickListen
     ArrayList<Task> taskList;
     KidTaskAdapter adapter;
     Button btn_next;
+    TextView txt_jour;
+    Calendar calendar;
+    Database bdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,12 @@ public class KidSchedule extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_kid_schedule);
         rw_sched = findViewById(R.id.rw_kidsched);
         btn_next = findViewById(R.id.btn_nxt);
+        txt_jour = findViewById(R.id.txt_jour);
+        calendar = Calendar.getInstance();
 
+        txt_jour.setText(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()));
+
+        bdd = new Database();
         taskList = new ArrayList<>();
         adapter = new KidTaskAdapter(taskList);
 
@@ -32,6 +46,32 @@ public class KidSchedule extends AppCompatActivity implements View.OnClickListen
         rw_sched.setLayoutManager(new LinearLayoutManager(this));
 
         btn_next.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bdd.readData(bdd.getUserRef().child("").child("Task"), new OnGetDataListener() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot:dataSnapshot.getChildren()
+                     ) {
+                    Task task = snapshot.getValue(Task.class);
+                    taskList.add(task);
+                    Toast.makeText(this, "ajout de ma tache", Toast.LENGTH_LONG).show();
+                    adapter.notifyDataSetChanged();
+                };
+
+            @Override
+            public void onStart() {
+                    Log.d("ONSTART", "Started");
+            }
+
+            @Override
+            public void onFailure() {
+
+            }
+        });
     }
 
     @Override
