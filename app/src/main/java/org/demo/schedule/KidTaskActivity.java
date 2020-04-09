f9e4fb02f9a45dbfdb07c355e18c2bc41ac804a4
HeyCi <acchevp@gmail.com>
2020-04-09 20:51:40 +0200

    affichage des infos de la tache

--- app/src/main/java/org/demo/schedule/KidTaskActivity.java
+++ app/src/main/java/org/demo/schedule/KidTaskActivity.java
@@ -3,13 +3,46 @@ package org.demo.schedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class KidTaskActivity extends AppCompatActivity {
+public class KidTaskActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txt_titre;
+    TextView txt_date;
+    TextView txt_heure;
+    Button btn_afinir;
+    Button btn_fini;
+    Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kid_task);
-        // TODO récupérer les données envoyées dans l'intent pour afficher la tâche
+        txt_titre = findViewById(R.id.txt_titre);
+        txt_date = findViewById(R.id.txt_date);
+        txt_heure = findViewById(R.id.txt_heure);
+        btn_afinir = findViewById(R.id.btn_afaire);
+        btn_fini = findViewById(R.id.btn_fini);
+
+        task = (Task)getIntent().getSerializableExtra("task");
+
+        txt_titre.setText(task.getName_task());
+        txt_date.setText(task.getDate());
+        txt_heure.setText(task.getHour());
+
+        btn_fini.setOnClickListener(this);
+        btn_afinir.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btn_afinir) {
+            Toast.makeText(this, "tache pas finie", Toast.LENGTH_LONG).show();
+        } else if(view == btn_fini) {
+            Toast.makeText(this, "tache finie, bravo !", Toast.LENGTH_LONG).show();
+        }
     }
 }
